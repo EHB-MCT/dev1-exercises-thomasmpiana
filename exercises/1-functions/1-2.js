4858baa11dc3da62d1efc99eeda894a0095be886
thomas mpiana <thomas.mpiana@student.ehb.be>
2022-10-05 11:47:01 +0200

    box

---
+++ exercises/1-functions/1-2.js
@@ -0,0 +1,36 @@
+"use strict"
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+
+box();
+stroke();
+
+function box() {
+    context.beginPath();
+    context.moveTo(50, 50);
+    context.lineTo(150, 50);
+    context.lineTo(150, 150);
+    context.lineTo(50, 150);
+    context.lineTo(50, 50);
+    context.lineWidth = 3
+    context.stroke();
+}
+
+function stroke() {
+    context.beginPath();
+    context.moveTo(50, 50);
+    context.lineTo(150, 150);
+    context.moveTo(150, 50);
+    context.lineTo(50, 150);
+    context.lineWidth = 1
+    context.strokeStyle = "red"
+    context.stroke();
+
+
+
+
+}
